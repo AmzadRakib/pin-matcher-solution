34b27a31e8e543d42aba691367cfb52a8240ea7b
AmzadRakibe <amzadhrakib570@gmail.comm>
2023-04-27 13:55:52 +0600

    everything is setup

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -18,3 +18,52 @@ document.getElementById('btn-generate').addEventListener('click', function () {
     const generatePinField = document.getElementById('genrate-pin-field');
     generatePinField.value = pin;
 })
+
+document.getElementById('calculator-body').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-number');
+    const currentTypedNumber = typedNumberField.value;
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typedNumberField.value = '';
+        }
+        else if (number === '<') {
+            const digits = currentTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+
+    else {
+        const updateTypedNumber = currentTypedNumber + number;
+        typedNumberField.value = updateTypedNumber;
+    }
+
+
+})
+
+
+document.getElementById('verify-pin').addEventListener('click', function () {
+    const generatePinField = document.getElementById('genrate-pin-field')
+    const currentPin = generatePinField.value;
+
+    const typedNumberField = document.getElementById('typed-number');
+    const typedNumber = typedNumberField.value;
+    generatePinField.value = '';
+    typedNumberField.value = '';
+    const pinCorrectMessage = document.getElementById('pin-correct');
+    const pinIncorrectMessage = document.getElementById('pin-incorrect');
+
+
+
+    if (currentPin === typedNumber) {
+        pinCorrectMessage.style.display = 'block';
+        pinIncorrectMessage.style.display = 'none';
+    }
+    else {
+        pinIncorrectMessage.style.display = 'block';
+        pinCorrectMessage.style.display = 'none';
+    }
+
+})
